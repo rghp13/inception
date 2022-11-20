48ba82575899d17984bb879b543888b1d3d8b177
rponsonn <rghp13@gmail.com>
2022-11-20 18:09:37 +0100

    murder

--- srcs/requirements/mariadb/conf/init.sh
+++ srcs/requirements/mariadb/conf/init.sh
@@ -4,22 +4,25 @@ if [ ! -d "/run/mysqld" ]; then
 	chown -R mysql:mysql /run/mysqld
 fi
 
-if [ ! -d "/var/lib/mysql/mysql"]; then
+if [ ! -d "/var/lib/mysql/mysql" ]; then
 	chown -R mysql:mysql /var/lib/mysql
-	mysql_install_db --basedir=/usr --datadir=/var/lib/mysql --user=mysql > /dev/null
+	mysql_install_db --basedir=/usr --datadir=/var/lib/mysql --user=mysql --rpm > /dev/null
 fi
-	/usr/lib/mysqld -u root << EOF
-UPDATE mysql.user SET Password=PASSWORD('123456') WHERE User='root';
+echo "RUNNING COMMAND"
+/usr/bin/mysqld -u mysql --bootstrap << EOF
+FLUSH PRIVILEGES;
+ALTER USER 'root'@'localhost' IDENTIFIED BY '123456';
 DELETE FROM mysql.user WHERE User='';
 DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
 DROP DATABASE IF EXISTS test;
 DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
-CREATE DATABASE IF NOT EXISTS wordpress;
-CREATE USER 'romain'@'%' IDENTIFIED BY 'password';
+CREATE DATABASE IF NOT EXISTS wordpress_db;
+CREATE USER 'wordpress_user'@'%' IDENTIFIED BY 'password';
+GRANT ALL PRIVILEGES ON wordpress_db.* TO 'wordpress_user'@'%';
 FLUSH PRIVILEGES;
 EOF
-#run init.sql
-#sed -i "s|skip-networking|# skip-networking|" /etc/my.cnf.d/mariadb-server.cnf
-#sed -i "s|.*bind-address\s*=.*|bind-address=0.0.0.0|g" /etc/my.cnf.d/mariadb-server.cnf
+echo "COMMAND DONE"
+sed -i "s|skip-networking|# skip-networking|" /etc/my.cnf.d/mariadb-server.cnf
+sed -i "s|.*bind-address\s*=.*|bind-address=0.0.0.0|g" /etc/my.cnf.d/mariadb-server.cnf
 echo "all good"
 exec mysqld --user=mysql --console
