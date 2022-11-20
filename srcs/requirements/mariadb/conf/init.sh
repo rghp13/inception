a2410f1d856117663c0330c60917bc79be6b9e17
Romain George Henri Ivan Ponsonnet <rghp13@gmail.com>
2022-11-20 16:46:07 +0100

    more testing

--- srcs/requirements/mariadb/conf/init.sh
+++ srcs/requirements/mariadb/conf/init.sh
@@ -4,12 +4,11 @@ if [ ! -d "/run/mysqld" ]; then
 	chown -R mysql:mysql /run/mysqld
 fi
 
-service mysql start
 if [ ! -d "/var/lib/mysql/mysql"]; then
-	mkdir -p /var/lib/mysql
 	chown -R mysql:mysql /var/lib/mysql
 	mysql_install_db --basedir=/usr --datadir=/var/lib/mysql --user=mysql > /dev/null
-	mysql -u root -p << EOF
+fi
+	/usr/lib/mysqld -u root << EOF
 UPDATE mysql.user SET Password=PASSWORD('123456') WHERE User='root';
 DELETE FROM mysql.user WHERE User='';
 DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
@@ -19,9 +18,8 @@ CREATE DATABASE IF NOT EXISTS wordpress;
 CREATE USER 'romain'@'%' IDENTIFIED BY 'password';
 FLUSH PRIVILEGES;
 EOF
-fi
 #run init.sql
-sed -i "s|skip-networking|# skip-networking|" /etc/my.cnf.d/mariadb-server.cnf
-sed -i "s|.*bind-address\s*=.*|bind-address=0.0.0.0|g" /etc/my.cnf.d/mariadb-server.cnf
+#sed -i "s|skip-networking|# skip-networking|" /etc/my.cnf.d/mariadb-server.cnf
+#sed -i "s|.*bind-address\s*=.*|bind-address=0.0.0.0|g" /etc/my.cnf.d/mariadb-server.cnf
 echo "all good"
 exec mysqld --user=mysql --console
